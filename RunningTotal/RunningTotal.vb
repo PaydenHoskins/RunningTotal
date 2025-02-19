7aac2cd325fec4ad302d873a5c257ca964657367
Payden Hoskins <paydenhoskins@gmail.com>
2025-02-19 09:00:40 -0700

    Added Tax and strdup

--- RunningTotal/RunningTotal.vb
+++ RunningTotal/RunningTotal.vb
@@ -22,18 +22,27 @@ Module RunningTotal
         MoveOn = False
         Do Until UserInput = "Q"
             Console.WriteLine("Input amount owed.")
+            Console.WriteLine()
             UserInput = Console.ReadLine()
             Do
                 Try
                     AmountOwed = CDec(UserInput)
                     MoveOn = True
                 Catch
+                    Console.WriteLine()
                     Console.WriteLine("not a number")
+                    Console.WriteLine()
                 End Try
-            Loop Until MoveOn = True
+                Console.WriteLine(StrDup(50, "*"))
+                Console.WriteLine("After Tax.")
+                AmountOwed = CDec(AmountOwed * 1.06)
+                Console.WriteLine((AmountOwed).ToString("c"))
+                Console.WriteLine()
+            Loop Until MoveOn = True Or UserInput = "Q"
             MoveOn = False
-            Console.WriteLine()
+            Console.WriteLine(StrDup(50, "*"))
             Console.WriteLine("Input amount paid.")
+            Console.WriteLine()
             UserInput = Console.ReadLine()
             Do
                 Do
@@ -41,21 +50,28 @@ Module RunningTotal
                         AmountPaid = CDec(UserInput)
                         MoveOn = True
                     Catch
+                        Console.WriteLine()
                         Console.WriteLine("not a number")
+                        Console.WriteLine()
                     End Try
-                Loop Until MoveOn = True
+                Loop Until MoveOn = True Or UserInput = "Q"
                 Console.WriteLine()
                 Console.WriteLine("Change Owed.")
                 RunningTotal(AmountOwed, "0")
                 Console.WriteLine(FinalTotal(AmountOwed, AmountPaid))
-                Console.WriteLine()
+                Console.WriteLine(StrDup(50, "*"))
                 Console.WriteLine("Would you like to see complete total of today?")
-                UserInput = Console.ReadLine
+                Console.WriteLine()
+                UserInput = Console.ReadLine()
+                Console.WriteLine()
                 If UserInput = "Yes" Then
-                    Console.WriteLine(RunningTotal(0, "1"))
+                    Console.WriteLine(StrDup(50, "*"))
+                    Console.WriteLine(RunningTotal(0, "1").ToString("c"))
+                    Console.WriteLine(StrDup(50, "*"))
                 ElseIf UserInput = "Zero" Then
-                    Console.WriteLine(RunningTotal(0, "2"))
-
+                    Console.WriteLine(StrDup(50, "*"))
+                    Console.WriteLine(RunningTotal(0, "2").ToString("c"))
+                    Console.WriteLine(StrDup(50, "*"))
                 End If
             Loop Until UserInput = "Q" Or MoveOn = True
         Loop
@@ -69,6 +85,7 @@ Module RunningTotal
 
     Function RunningTotal(ByRef OwedTotal As Decimal, ByRef Operation As String) As Decimal
         Static AddedTotal As Decimal
+        Static FinalTotal As Decimal
         Select Case Operation
             Case "0"
                 AddedTotal += OwedTotal
@@ -77,6 +94,8 @@ Module RunningTotal
             Case "2"
                 AddedTotal -= AddedTotal
         End Select
-        Return AddedTotal
+        FinalTotal = (AddedTotal * -1)
+        Return FinalTotal
     End Function
+
 End Module
